75777767082271d9e7298d0b8ca6b9c1fc9bc87a
Stewie Trader <es@papartners.co>
2025-10-14 17:59:55 -0400

    Homepage: reduce DataroomPrompt delay to 5s

--- pa-partners-new/src/app/page.tsx
+++ pa-partners-new/src/app/page.tsx
@@ -8,7 +8,7 @@ import DataroomPrompt from "@/components/common/DataroomPrompt";
 export default function Home() {
   return (
     <>
-      <DataroomPrompt delayMs={10000} />
+      <DataroomPrompt delayMs={5000} />
       <Hero />
       <ServicesOverview />
       <PortfolioGrid />
